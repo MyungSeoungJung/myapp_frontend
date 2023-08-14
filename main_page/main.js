0bdea81f4272f06180bd13087b4241b7dfc7de6f
msj <audtmdwjd@naver.com>
2023-08-14 18:12:48 +0900

    추천프로그램 창

--- main_page/main.js
+++ main_page/main.js
@@ -1,4 +1,5 @@
-
+const recommend_program = document.querySelector("#recommend_program");
+const recommend_program_select = document.querySelectorAll(".program_container");
 (async() => {
     const response = await fetch ("http://localhost:8080/user/main",{
         headers : {
@@ -11,9 +12,13 @@
     console.log(result);
 
     username.textContent =  result.name + "님";
-    // if (result.programName == null) {
-    //     alert("프로그램을 선택하지않으셨습니다")
-        
-    // }
+
+    // -------------추천 프로그램 띄우는 코드--------------------
+    recommend_program.style.display ="none";
+    
+    if (result.programName == null) {
+        // alert("프로그램이 선택되지 않았습니다 추천 프로그램을 띄워드릴게요")
+        recommend_program.style.display ="block";
+    }
 })(); // 즉시실행 
 
